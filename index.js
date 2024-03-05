75c54b73ada31f77ded886bbb7b9151300b7a56f
Danish Bansal <danishbansal60@gmail.com>
2024-03-05 13:10:13 -0500

    no-matter change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/mysqlData',(res, req, next) => {
 const port_number = process.env.PORT || 3000
 
 app.get('/',(res, req, next) => {
-    res.body("hi this is response page")
+    res.send("hi this is response page")
 })
 
 app.listen(port_number, () => {
